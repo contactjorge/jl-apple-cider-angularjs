5a8836281a3780ef1375d550e8cfbc6c886b6734
contactjorge <contactjorge@thejorgelopez.com>
2018-01-31 20:22:12 -0600

    commit

--- app/js/controllers/allControllers.js
+++ app/js/controllers/allControllers.js
@@ -2,11 +2,10 @@ var allControllers = angular.module('allControllers', []);
 
 allControllers.controller('ProductsController',
 	function ProductController($scope, $http){
-		var productURL = 'https://api.mlab.com/api/1/databases/wondersign/collections/products?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
-		var productURL = 'https://api.mlab.com/api/1/databases/wondersign/collections/products/59644b3df36d286340b27f66?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
-		
+		var productsURL = 'https://api.mlab.com/api/1/databases/wondersign/collections/products?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
+		var aProductURL = 'https://api.mlab.com/api/1/databases/wondersign/collections/products/59644b3df36d286340b27f66?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
 		var products = [];
-		$http.get(productURL).then(function (response){
+		$http.get(productsURL).then(function (response){
 			$scope.products = response.data;
 			$scope.list = $scope.products;
 		});
@@ -15,13 +14,17 @@ allControllers.controller('ProductsController',
 		console.log("id: " + stringyfied._id);
 		
 		console.log ($scope.list);
+		/*var updateURL = 'https://api.mlab.com/api/1/databases/wondersign/collections/products/59644b3df36d286340b27f66?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
+		$http.post(aProductURL).then(function (response){
+			$scope.postProduct = { _id:$scope.products._id, name:$scope.name, sku:$scope.sku };
+		});
 		$scope.addProduct = function(){
-			$scope.product.push({ _id:$scope.products._id, name:$scope.name, sku:$scope.sku });
+			$scope.products.push({ _id:$scope.products._id, name:$scope.name, sku:$scope.sku });
 		};
 		
 		$scope.getAllProducts = function(){
 			return $scope.products.length;
 		};
-		
+		*/
 		
 	});
